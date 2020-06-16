088bf98edbdd55b4545bf40d092a1786a33b0aa6
Adrien Robert <adrien.robert@skoove.com>
2020-06-16 16:48:43 +0200

    Add marker component

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Deed code challenge
 Coding steps:
 1. Create git repo
 2. Create react app via 
+
+Sources: 
+Markers: https://gist.github.com/jobsamuel/56496033bfb4d0f3e316aeb88341ed16
---
+++ src/Components/Marker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../Styles/Marker.css';
+
+function Marker(props) {
+    return <>
+        <div className="pin"></div>
+        <div className="pulse"></div>
+    </>
+};
+
+export default Marker;
